2f5b6a39c65ee937b27131b3d909a908ecdc46c1
Antón Vega <anton.paidoslalin@gmail.com>
2022-02-18 13:53:41 +0100

    nuevotodo and probes in src/index.js

---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,20 @@
+export class TodoList{
+    constructor(){
+        
+        this.todos=[];
+    }
+    nuevoTodo (todo){
+        this.todos.push(todo);
+    }
+    eliminarTodo (id){
+       // todos.array.forEach();
+    
+
+    }
+    marcarCompletado(id){
+
+    }
+    eliminarCompletados(){
+
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import { Todo } from './classes/todo.class';
+import { TodoList } from './classes/todo-list.class';
 import './styles.css';
 
+
+
+
 const tarea=new Todo('Aprender Javascript');
+const tarea2=new Todo('Domar un unicornio')
+const todolist=new TodoList();
+
 console.log(tarea);
+todolist.nuevoTodo(tarea);
+todolist.nuevoTodo(tarea2);
+console.log(todolist);
