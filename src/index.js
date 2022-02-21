0171c46bafef04b82288fb8bd4a6cf725eddb2ce
Antón Vega <anton.paidoslalin@gmail.com>
2022-02-21 09:52:06 +0100

    Modificada en index.js / index.js modified

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { Todo } from './classes/todo.class';
 import { TodoList } from './classes/todo-list.class';
 import './styles.css';
-
+let a='';
 
 
 
 const tarea=new Todo('Aprender Javascript');
+a = prompt('Introduza  unha letra: ');
 const tarea2=new Todo('Domar un unicornio')
 const todolist=new TodoList();
 
