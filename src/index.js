ea1fca8cb0fc516d927cfc6561ed4336bebe3af5
Antón Vega <anton.paidoslalin@gmail.com>
2022-02-23 12:55:04 +0100

    docummenting a bit index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { crearTodoHtml } from './js/componentes';
 
 export const todoList=new TodoList();
 
+//Datos de exemplo
 const tarea=new Todo('Aprender Javascript');
 
 todoList.nuevoTodo(tarea);
 
 console.log(todoList);
-
 
 crearTodoHtml(tarea);
