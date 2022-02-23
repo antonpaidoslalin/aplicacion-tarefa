c5fdc8ec193550f1fc266fef73a90bae025db5b7
Antón Vega <anton.paidoslalin@gmail.com>
2022-02-23 10:40:12 +0100

    correct a small problem in index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import { Todo } from './classes/todo.class';
 import { TodoList } from './classes/todo-list.class';
 import { crearTodoHtml } from './js/componentes.js';
 import './styles.css';
+import { SingleEntryPlugin } from 'webpack';
 
 const tarea=new Todo('Aprender Javascript');
 const tarea2=new Todo('Domar un unicornio')
 export const todoList=new TodoList();
-
 todoList.nuevoTodo(tarea);
 todoList.nuevoTodo(tarea2);
 crearTodoHtml ( tarea );
